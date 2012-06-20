62b0dbb39cb00c97a03c03aa09945d59d70ff6b9
Ada Yu <bhappy@gmail.com>
2012-06-19 23:36:16 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loxopix::Application.config.secret_token = '21a4b4b16f0932c2246ab25d134a87d018c1e1325dcf61f10c9d9075a42a9f4030c6b4ff01843d197b2e2c3f50affa61b79c2ce943b701e53eadaaf1ff9fdd16'
